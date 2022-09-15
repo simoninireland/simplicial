d4e2079798ec2d0aecad517df9cf76e5b6c8b109
Simon Dobson <simon.dobson@computer.org>
2022-09-15 15:55:20 +0100

    Added comment

--- test/test_simplicialcomplex.py
+++ test/test_simplicialcomplex.py
@@ -416,7 +416,7 @@ class SimplicialComplexTests(unittest.TestCase):
         ns = c.relabel(lambda s: s + 1000)
         self.assertCountEqual(ns.values(), [ 1001, 1002, 1003,
                                              1012, 1013, 1023,
-                                             1123 ])
+                                             1123 ])  # not 1 or 123, as they're not re-written
         self.assertCountEqual(ns.values(), c.simplices())
         self.assertCountEqual(c.faces(1123), [ 1012, 1013, 1023 ])
         self.assertCountEqual(c.faces(1012), [ 1001, 1002 ])
