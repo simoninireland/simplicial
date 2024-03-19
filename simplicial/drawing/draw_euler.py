fdc09c9773d9a7d472369bfb25a8b6a19e3b7119
Simon Dobson <simon.dobson@computer.org>
2024-03-19 16:17:33 +0000

    Updated signature to work with simplicial functions

--- simplicial/drawing/draw_euler.py
+++ simplicial/drawing/draw_euler.py
@@ -18,13 +18,14 @@
 # along with Simplicial. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
 from copy import deepcopy
-from simplicial import SimplicialComplex, Embedding
+from simplicial import SimplicialComplex, Embedding, SimplicialFunction
 from simplicial.drawing import patchesForComplex
 
 
 def drawEulerIntegral(c: SimplicialComplex, em: Embedding,
+                      sf: SimplicialFunction[int],
                       axs,
-                      attr = 'count', backgroundColour = '0.95',
+                      backgroundColour = '0.95',
                       subfieldXY = None, subfieldWH = None,
                       simplexColour = None, simplexSize = 0.02,
                       showLevelSets = True,
@@ -43,8 +44,8 @@ def drawEulerIntegral(c: SimplicialComplex, em: Embedding,
 
     :param c: the complex
     :param em: embedding providing the positions of the 0-simplices
+    :param sf: the metric
     :param axs: the axes to draw each level set into
-    :param attr: the attribute holding the height information (default 'count')
     :param backgroundColour: the background colour for the field (default '0.95')
     :param subfieldXY: the bottom-left corner of the sub-field to draw (default all)
     :param subfieldWH: the width and heigh of the sub-field to draw (default all)
@@ -66,8 +67,8 @@ def drawEulerIntegral(c: SimplicialComplex, em: Embedding,
     if subfieldWH is None:
         subfieldWH = [1.0 - subfieldXY[0], 1.0 - subfieldXY[1]]
 
-    # extract the maximum height and sanbitry-check the axes
-    maxHeight = max([c[s][attr] for s in c.simplicesOfOrder(0)])
+    # extract the maximum height and sanity-check the axes
+    maxHeight = max([sf[s] for s in c.simplicesOfOrder(0)])
     if len(axs) < maxHeight + 1:
         raise ValueError(f'Not enough axes to draw integral ($h_{max} = {maxHeight}$)')
 
@@ -75,7 +76,7 @@ def drawEulerIntegral(c: SimplicialComplex, em: Embedding,
     h = -1
     for i in range(len(axs)):
         ax = axs[i]
-        bs = [s for s in c.simplicesOfOrder(0) if c[s][attr] > h]
+        bs = [s for s in c.simplicesOfOrder(0) if sf[s] > h]
         levelSet = deepcopy(c).restrictBasisTo(bs)
         pcs = patchesForComplex(levelSet, em, simplexColour, simplexSize)
         for pc in pcs:
@@ -84,7 +85,7 @@ def drawEulerIntegral(c: SimplicialComplex, em: Embedding,
         # add metric
         if showMetrics:
             for s in c.simplicesOfOrder(0):
-                ax.annotate(c[s][attr],
+                ax.annotate(sf[s],
                             xy=em[s],
                             xytext=(-simplexSize, -simplexSize), textcoords='offset pixels',
                             color=metricsColour, fontsize=metricsFontSize)
